0b849d30b2e0e71f6f9e56c41aaa2ae407a9dc56
Royal Dsouza <royald000@gmail.com>
2025-05-01 12:33:45 +0530

    updated the table name

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ Configuration parameters for the Sales ETL microservice.
 # BigQuery settings
 PROJECT_ID = "elevated-column-458305-f8"
 DATASET_ID = "Sales_Transaction_HTTP"
-TABLE_NAME = "transactions"
+TABLE_NAME = "transaction"
 
 # Fully qualified BigQuery table ID
 BIGQUERY_TABLE_ID = f"{PROJECT_ID}.{DATASET_ID}.{TABLE_NAME}"
